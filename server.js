4ffe99dbd437b6f2a95120b7e4bf195e6469e004
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-04-01 11:36:12 +0530

    Basic Server setup
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by bhavyaagg on 01/04/18.
+ */
+
+const express = require('express');
+const app = express();
+
+const http = require('http');
+const server = http.Server(app)
+
+const socketIO = require('socket.io');
+const io = socketIO(server);
+
+
+app.use(express.static(__dirname + '/public'));
+
+server.listen(4444, () => {
+  console.log("Server listening on port: 4444");
+})
