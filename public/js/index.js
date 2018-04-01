422f8ec72c86e0472a144cd2f25c03befeee86f6
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-04-01 12:03:32 +0530

    Draw line on canvas
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,40 @@
 /**
  * Created by bhavyaagg on 01/04/18.
  */
+
+$(document).ready(() => {
+  const socket = io();
+  const canvas = document.getElementById('whiteboard');
+  const context = canvas.getContext('2d');
+  let drawing = false;
+  
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  
+  function drawLine(x1, y1, x2, y2) {
+    
+    context.beginPath();
+    context.lineWidth = 2;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+    
+  }
+  
+  drawLine(80, 80, 100, 100);
+  
+  
+  // canvas.addEventListener('mousedown', onMouseDown);
+  // canvas.addEventListener('mouseup', onMouseUp);
+  // canvas.addEventListener('mouseout', onMouseUp);
+  // canvas.addEventListener('mousemove', onMouseMove);
+  //
+  //
+  //
+  // function onMouseDown() {
+  //
+  // }
+  //
+  
+})
